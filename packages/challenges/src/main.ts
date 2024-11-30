5531a9628f58531b443d36f8992dae4853c1cb50
Miguel Soares de Oliveira <miguelosoares1@hotmail.com>
2024-11-30 14:19:50 -0300

    feat(challenge): initial main module creation

---
+++ packages/challenges/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: false,
+      validateCustomDecorators: true,
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, () => {
+    console.info(`[‼] Server started at port ${port}!`);
+  });
+}
+bootstrap();
