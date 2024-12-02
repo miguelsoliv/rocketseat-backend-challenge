9e80724bcf417b02d5456706a04d8025ce3a7e0d
Miguel Soares de Oliveira <miguelosoares1@hotmail.com>
2024-12-02 10:21:27 -0300

    feat: add prisma and initial db module

--- packages/challenges/src/challenges/challenges.module.ts
+++ packages/challenges/src/challenges/challenges.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChallengesService } from './challenges.service';
 import { ChallengesResolver } from './challenges.resolver';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [ChallengesResolver, ChallengesService],
 })
 export class ChallengesModule {}
---
+++ packages/challenges/src/database/migrations/20241201024330_initial_setup/migration.sql
@@ -0,0 +1,29 @@
+-- To be able to use uuid_generate_v4()
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- CreateEnum
+CREATE TYPE "answer_status_enum" AS ENUM ('Pending', 'Error', 'Done');
+
+-- CreateTable
+CREATE TABLE "challenges" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "challenges_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "answers" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "repositoryUrl" TEXT NOT NULL,
+    "status" "answer_status_enum" NOT NULL,
+    "note" INTEGER NOT NULL,
+    "challengeId" UUID NOT NULL,
+
+    CONSTRAINT "answers_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "answers" ADD CONSTRAINT "answers_challengeId_fkey" FOREIGN KEY ("challengeId") REFERENCES "challenges"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
